65599011317c4ec001db432daaf2e22858747582
Dmitriy Senko <d.senko@dodobrands.io>
2025-05-05 11:24:51 +0300

    just for test

--- pkg/provider/yandex/lockbox/client/grpcclient.go
+++ pkg/provider/yandex/lockbox/client/grpcclient.go
@@ -18,7 +18,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/external-secrets/external-secrets/pkg/provider/yandex/common"
-	"github.com/keeper-security/secrets-manager-go/core/logger"
 	api "github.com/yandex-cloud/go-genproto/yandex/cloud/lockbox/v1"
 	"github.com/yandex-cloud/go-sdk/iamkey"
 	"google.golang.org/grpc"
@@ -63,8 +62,8 @@ func (c *grpcLockboxClient) GetPayloadEntries(ctx context.Context, iamToken, fol
 	if folderID != "" {
 		payloadEntry, err := c.GetSecretByName(ctx, iamToken, folderID, secretIDOrName, versionID)
 		if err != nil {
-			logger.Error(err)
-			return nil, fmt.Errorf("method done with error - %s. Properties are :method is %s, folderId: %s, versionId: %s, secretIdOrName: %s", err.Error(), "GetSecretByName", folderID, versionID, secretIDOrName)
+			//return nil, fmt.Errorf("method done with error - %s. Properties are :method is %s, folderId: %s, versionId: %s, secretIdOrName: %s", err.Error(), "GetSecretByName", folderID, versionID, secretIDOrName)
+			return nil, fmt.Errorf("GetSecretByName")
 		}
 
 		return payloadEntry, nil
@@ -73,7 +72,8 @@ func (c *grpcLockboxClient) GetPayloadEntries(ctx context.Context, iamToken, fol
 	// If the folderID is not provided in the SecretStore, we can attempt to retrieve the secret by its ID
 	payloadEntry, err := c.GetSecretById(ctx, iamToken, secretIDOrName, versionID)
 	if err != nil {
-		return nil, fmt.Errorf("method done with error - %s. Properties are :method is %s, folderId: %s, versionId: %s, secretIdOrName: %s", err.Error(), "GetSecretById", folderID, versionID, secretIDOrName)
+		//return nil, fmt.Errorf("method done with error - %s. Properties are :method is %s, folderId: %s, versionId: %s, secretIdOrName: %s", err.Error(), "GetSecretById", folderID, versionID, secretIDOrName)
+		return nil, fmt.Errorf("GetSecretById")
 	}
 
 	return payloadEntry, nil
