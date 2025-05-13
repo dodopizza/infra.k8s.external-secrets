944c39e2038d6aa82899aa3b112b982a4eac0d86
Dmitriy Senko <d.senko@dodobrands.io>
2025-05-13 09:15:30 +0300

    Added folderId property

--- apis/externalsecrets/v1/secretstore_yandexcertificatemanager_types.go
+++ apis/externalsecrets/v1/secretstore_yandexcertificatemanager_types.go
@@ -34,6 +34,10 @@ type YandexCertificateManagerProvider struct {
 	// +optional
 	APIEndpoint string `json:"apiEndpoint,omitempty"`
 
+	//If provided sets the ability to get secrets by its name in the specified folder
+	// +optional
+	FolderID string `json:"folderId,omitempty"`
+
 	// Auth defines the information necessary to authenticate against Yandex Certificate Manager
 	Auth YandexCertificateManagerAuth `json:"auth"`
 
